7f6aeb874fb776cb9e8c04c3cbbd50fc7f075cde
tabithaamanor <103070609+tabithaamanor@users.noreply.github.com>
2023-02-09 20:09:28 +0000

    update

--- SoyoApp/src/Main.java
+++ SoyoApp/src/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        while (true) {
+        while (false) {
             System.out.print("Enter Customer Name: ");
             customers.name = scanner.nextLine();
 
